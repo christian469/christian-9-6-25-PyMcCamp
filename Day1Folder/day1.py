781e2f6cdeace8b25f8cebab41b2f65ab8f2f9f1
christian69 <christianisapro28@gmail.com>
2025-06-12 01:56:23 +0000

    Jun 12, 2025, 1:56 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,41 +1,50 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+print("my name is christian")
+print("steve's lava chicken")
 
 ########################################################################
 # Task 1:
-
-
+print("I am feeling good") 
+ 
 
 ########################################################################
 # Task 2:
-
-
-
+ 
 ########################################################################
 # Task 3:
-
-
-
+animal1="tiger"
+animal2="panda"
+animal3="benover"
+print(animal1)
+print(animal2)
+print(animal3)
 ########################################################################
 # Task 4:
-
-
-
+y = 36 
+x = 89
+print (y + x )
+print (y / x / 5 * 60 - 90  )
 ########################################################################
 # Task 5:
-
-
-
+name = "my dog"
+food = " ate my homework"
+print(name)
+print(food)
 ########################################################################
 # Task 6:
-
-
-
+def intro():   
+   print("my name is christian")
+   print("my age is 13")
+   print("my school is st andrew")
+   print("if i had $2000 i would use $1600 to bux robux and $400 on food ")
+intro()   
 ########################################################################
 # Task 7:
+def addition(num1, num2):
+    print( num1 + num2 )
 
-
-
+addition(200098, 39082)
+addition(2986, 57204)
 ########################################################################
 # Additional exercises:
--- Day2Folder/minecraft_day2.py
+++ Day2Folder/minecraft_day2.py
@@ -1,2 +1,44 @@
 # AT THE END OF LESSON, COPY YOUR CODE FROM MINECRAFT HERE.
 # THIS IS SO THAT YOU HAVE A RECORD OF YOUR CODE FROM MINECRAFT CODE BUILDER
+def on_chat():
+    agent.teleport_to_player()
+player.on_chat("tp", on_chat)
+
+
+def turnleft():
+    agent.turn(TurnDirection.LEFT)
+    
+def on_chat2():
+    pass
+player.on_chat("tl", turnleft)
+
+def turnright():
+    agent.turn(RIGHT)
+
+player.on_chat("tr", turnright)
+
+def moveUp():
+    agent.move(UP, 1)
+
+def on_chat3():
+    pass
+player.on_chat("fly", on_chat3)
+
+def mvdown():
+    agent.move(DOWN, 1)
+    def on_chat4():
+        pass
+player.on_chat("down", mvdown)
+
+def obby1():
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    agent.move(FORWARD, 1)
+    agent.move(DOWN, 1)
+    pass
+player.on_chat("obby1", obby1)
+
+def obby2():
+    agent.move(BACK, 1)
+
+player.on_chat("BACK", obby2)
